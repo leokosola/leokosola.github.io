a927581ced507e809aeaa7cfdec784dda261f79d
kosolal1 <leo.kosola@aalto.fi>
2019-03-22 01:35:46 +0200

    väliaikainen sivu jossa linkki pdf portfolioon

--- sketsi.js
+++ sketsi.js
@@ -5,15 +5,15 @@ boksi1 = document.getElementById("box1");
 boksi2 = document.getElementById("box2");
 boksi3 = document.getElementById("box3");
 boksi4 = document.getElementById("box4");
-boksi5 = document.getElementById("box5");
+// boksi5 = document.getElementById("box5");
 
 let x1 = 0;
-let x2 = x1 + 100;
-let x3 = x2 + 100;
-let x4 = x3 + 100;
-let x5 = x4 + 100;
+let x2 = x1 + 150;
+let x3 = x2 + 150;
+let x4 = x3 + 150;
+// let x5 = x4 + 150;
 
-let speed = 1.2;
+let speed = 1.5;
 
 
 
@@ -21,6 +21,10 @@ function setup() {
   width = karuselli.offsetWidth;
   height = karuselli.offsetHeight;
   createCanvas(width, height).parent("karuselli");
+
+  print("Welcome to my website.")
+  print("Sadly, this is currently under construction -- ")
+  print("Feel free to send me an email leo kosola aalto fi")
 }
 
 function draw() {
@@ -31,7 +35,7 @@ function draw() {
     x1 = x1 - speed;
    }
  
-   if (x1 < 0 - 150) {
+   if (x1 < 0 - 200) {
      x1 = width - 1;
     }
 
@@ -41,8 +45,8 @@ function draw() {
     x2 = x2 - speed;
    }
  
-   if (x2 < 0 - 250) {
-     x2 = width - 100;
+   if (x2 < 0 - 350) {
+     x2 = width - 150;
     }
 
   boksi3.style.left = x3;
@@ -51,8 +55,8 @@ function draw() {
     x3 = x3 - speed;
    }
  
-   if (x3 < 0 - 350) {
-     x3 = width - 200;
+   if (x3 < 0 - 500) {
+     x3 = width - 300;
     }
 
   boksi4.style.left = x4;
@@ -61,21 +65,18 @@ function draw() {
     x4 = x4 - speed;
    }
  
-   if (x4 < 0 - 450) {
-     x4 = width - 300;
+   if (x4 < 0 - 650) {
+     x4 = width - 450;
     }
 
-  boksi5.style.left = x5;
+  // boksi5.style.left = x5;
 
-  if (x5 < width) {  
-    x5 = x5 - speed;
-   }
+  // if (x5 < width) {  
+  //   x5 = x5 - speed;
+  //  }
  
-   if (x5 < 0 - 550) {
-     x5 = width - 400;
-    }
-
-print(x4);
-
+  //  if (x5 < 0 - 800) {
+  //    x5 = width - 600;
+  //   }
 
 }
